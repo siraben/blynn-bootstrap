fec38c0881b4870f45967f9fa8ae43a05af55f78
Ben Siraphob <bensiraphob@gmail.com>
2026-05-06 16:38:22 -0700

    Handle hcc conditional preprocessing

--- vendor/hcc/Hcc/Preprocessor.hs
+++ vendor/hcc/Hcc/Preprocessor.hs
@@ -14,27 +14,105 @@ data PreprocessError = PreprocessError SrcPos String
 data Macro = ObjectMacro String [Token]
   deriving (Eq, Show)
 
-preprocess :: [Token] -> Either PreprocessError [Token]
-preprocess toks = go [] toks [] where
-  go macros rest acc = case rest of
-    [] -> Right (reverse acc)
-    Token sp (TokDirective text):xs -> handleDirective macros sp text xs acc
-    Token sp (TokIdent name):xs -> case lookupObject name macros of
-      Just body -> go macros xs (reverse (relocate sp body) ++ acc)
-      Nothing -> go macros xs (Token sp (TokIdent name):acc)
-    x:xs -> go macros xs (x:acc)
+data IfFrame = IfFrame
+  { ifParentActive :: Bool
+  , ifBranchTaken :: Bool
+  , ifActive :: Bool
+  }
+  deriving (Eq, Show)
 
-  handleDirective macros sp text xs acc = case directiveWords text of
+preprocess :: [Token] -> Either PreprocessError [Token]
+preprocess toks = go [] [] toks [] where
+  go macros frames rest acc = case rest of
+    [] ->
+      if null frames
+      then Right (reverse acc)
+      else Left (PreprocessError (SrcPos 1 1) "unterminated conditional directive")
+    Token sp (TokDirective text):xs -> handleDirective macros frames sp text xs acc
+    Token sp (TokIdent name):xs ->
+      if currentActive frames
+      then case lookupObject name macros of
+        Just body -> go macros frames xs (reverse (relocate sp body) ++ acc)
+        Nothing -> go macros frames xs (Token sp (TokIdent name):acc)
+      else go macros frames xs acc
+    x:xs ->
+      if currentActive frames
+      then go macros frames xs (x:acc)
+      else go macros frames xs acc
+
+  handleDirective macros frames sp text xs acc = case directiveWords text of
     "#define":name:body ->
-      defineObject macros sp name (unwords body) >>= \macros' -> go macros' xs acc
-    "#undef":name:_ -> go (undefObject name macros) xs acc
-    "#":_ -> go macros xs acc
-    [] -> go macros xs acc
-    word:_ | word `elem` ignoredDirectives -> go macros xs acc
-    word:_ -> Left (PreprocessError (spanStart sp) ("unsupported directive: " ++ word))
+      if currentActive frames
+      then defineObject macros sp name (unwords body) >>= \macros' -> go macros' frames xs acc
+      else go macros frames xs acc
+    "#undef":name:_ ->
+      if currentActive frames
+      then go (undefObject name macros) frames xs acc
+      else go macros frames xs acc
+    "#ifdef":name:_ ->
+      go macros (pushIf frames (isDefined name macros)) xs acc
+    "#ifndef":name:_ ->
+      go macros (pushIf frames (not (isDefined name macros))) xs acc
+    "#if":expr ->
+      evalIf macros expr >>= \cond -> go macros (pushIf frames cond) xs acc
+    "#elif":expr ->
+      evalIf macros expr >>= \cond -> replaceElif sp frames cond >>= \frames' -> go macros frames' xs acc
+    "#else":_ ->
+      replaceElse sp frames >>= \frames' -> go macros frames' xs acc
+    "#endif":_ ->
+      popIf sp frames >>= \frames' -> go macros frames' xs acc
+    "#":_ -> go macros frames xs acc
+    [] -> go macros frames xs acc
+    word:_ | word `elem` ignoredDirectives -> go macros frames xs acc
+    word:_ ->
+      if currentActive frames
+      then Left (PreprocessError (spanStart sp) ("unsupported directive: " ++ word))
+      else go macros frames xs acc
 
 ignoredDirectives :: [String]
 ignoredDirectives = ["#line", "#pragma"]
+
+currentActive :: [IfFrame] -> Bool
+currentActive frames = case frames of
+  [] -> True
+  frame:_ -> ifActive frame
+
+pushIf :: [IfFrame] -> Bool -> [IfFrame]
+pushIf frames cond = IfFrame parent cond (parent && cond) : frames where
+  parent = currentActive frames
+
+replaceElse :: Span -> [IfFrame] -> Either PreprocessError [IfFrame]
+replaceElse sp frames = case frames of
+  [] -> Left (PreprocessError (spanStart sp) "#else without #if")
+  IfFrame parent taken _:rest ->
+    Right (IfFrame parent True (parent && not taken) : rest)
+
+replaceElif :: Span -> [IfFrame] -> Bool -> Either PreprocessError [IfFrame]
+replaceElif sp frames cond = case frames of
+  [] -> Left (PreprocessError (spanStart sp) "#elif without #if")
+  IfFrame parent taken _:rest ->
+    Right (IfFrame parent (taken || cond) (parent && not taken && cond) : rest)
+
+popIf :: Span -> [IfFrame] -> Either PreprocessError [IfFrame]
+popIf sp frames = case frames of
+  [] -> Left (PreprocessError (spanStart sp) "#endif without #if")
+  _:rest -> Right rest
+
+evalIf :: [Macro] -> [String] -> Either PreprocessError Bool
+evalIf macros wordsAfterIf = case wordsAfterIf of
+  ["0"] -> Right False
+  ["1"] -> Right True
+  ["defined", name] -> Right (isDefined name macros)
+  [word] | "defined(" `startsWith` word && last word == ')' ->
+    Right (isDefined (take (length word - 9) (drop 8 word)) macros)
+  [name] -> case lookupObject name macros of
+    Just [Token _ (TokInt "0")] -> Right False
+    Just _ -> Right True
+    Nothing -> Right False
+  _ -> Left (PreprocessError (SrcPos 1 1) ("unsupported #if expression: " ++ unwords wordsAfterIf))
+
+startsWith :: String -> String -> Bool
+startsWith prefix text = take (length prefix) text == prefix
 
 defineObject :: [Macro] -> Span -> String -> String -> Either PreprocessError [Macro]
 defineObject macros sp name body =
@@ -47,6 +125,11 @@ lookupObject name macros = case macros of
   [] -> Nothing
   ObjectMacro macroName body:rest ->
     if name == macroName then Just body else lookupObject name rest
+
+isDefined :: String -> [Macro] -> Bool
+isDefined name macros = case lookupObject name macros of
+  Just _ -> True
+  Nothing -> False
 
 undefObject :: String -> [Macro] -> [Macro]
 undefObject name macros = case macros of
--- vendor/hcc/test/lexer-smoke.c
+++ vendor/hcc/test/lexer-smoke.c
@@ -1,8 +1,13 @@
 #define VALUE 42
+#define ENABLE_SUM 1
 
 int main(int argc, char **argv) {
   const char *msg = "hello\n";
+#if ENABLE_SUM
   int sum = 1+2 + 0xffUL;
+#else
+  int sum = 0;
+#endif
   /* block comment */
   if (argc >= 2 && argv[1] != 0) {
     return VALUE + msg[0] + sum;
