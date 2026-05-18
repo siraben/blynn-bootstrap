b9c43f874910b8d698e43a750e538b9ccc9fb97d
Ben Siraphob <bensiraphob@gmail.com>
2026-05-17 20:56:12 -0700

    Exercise recursive calls in committed MLC bytecode

--- mlc/mlc.ml
+++ mlc/mlc.ml
@@ -8,9 +8,10 @@
 
 type output = Done | Emit of int
 
+let rec select n = if n == 0 then 79 else select (n - 1) in
 let pair = (79, 10) in
 let (letter, newline) = pair in
-let first = Emit letter in
+let first = Emit (select 2) in
 let _ = write_byte (match first with Emit value -> value | _ -> 88) in
 let _ = write_string "K" in
 write_byte newline
