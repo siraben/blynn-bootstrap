25dcdff62a14a7b6bc6a753df267db7c850e5888
Ben Siraphob <bensiraphob@gmail.com>
2026-05-17 21:02:13 -0700

    Exercise byte values in committed MLC bytecode

--- mlc/mlc.ml
+++ mlc/mlc.ml
@@ -11,7 +11,10 @@ type output = Done | Emit of int
 let rec select n = if n == 0 then 79 else select (n - 1) in
 let pair = (79, 10) in
 let (letter, newline) = pair in
+let text = "K" in
+let buffer = Bytes.create 1 in
+buffer.[0] <- newline;
 let first = Emit (select 2) in
 let _ = write_byte (match first with Emit value -> value | _ -> 88) in
-let _ = write_string "K" in
-write_byte newline
+let _ = write_byte text.[0] in
+write_byte buffer.[0]
