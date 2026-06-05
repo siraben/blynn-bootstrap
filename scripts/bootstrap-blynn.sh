6896ea85488d2de38efcb8e4efbcb7ee72b541d7
Ben Siraphob <bensiraphob@gmail.com>
2026-06-04 22:09:30 -0700

    Print portable bootstrap TinyCC version

--- scripts/bootstrap-blynn.sh
+++ scripts/bootstrap-blynn.sh
@@ -41,4 +41,5 @@ M2_ARCH=$ARCH
 M2_OS=$OPERATING_SYSTEM
 SCRIPT_DIR=$script_dir
 export ARCH OPERATING_SYSTEM M2_ARCH M2_OS OUT_DIR SCRIPT_DIR
-exec sh "$script_dir/bootstrap-blynn.kaem"
+sh "$script_dir/bootstrap-blynn.kaem"
+"$OUT_DIR/tinycc-boot-hcc/bin/tcc" -version
